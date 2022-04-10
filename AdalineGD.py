27338e8050500a06fe1cbe81ed2b3e2dfd2aeefd
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2022-04-10 22:16:47 +0900

    last

--- AdalineGD.py
+++ AdalineGD.py
@@ -1,3 +1,5 @@
+from re import X
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -123,7 +125,7 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
                     c=colors[idx],
                     marker=markers[idx],
                     label=cl,
-                    edgecolor='black')
+                    edgecolors='black')
 
 
 # 決定領域のプロット
@@ -133,5 +135,86 @@ plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 # 凡例の設定（左上に配置）
 plt.legend(loc='upper left')
+# 図の表示
+plt.show()
+
+
+class AdalineGD(object):
+    # AdAptive LInear NEuron 分類器
+
+    # 学習率(0.0より大きく1.0以下の値)
+    eta: float
+    # 訓練データの訓練回数
+    n_iter: int
+    # 重みを初期化するための乱数シード
+    random_state: int
+
+    # w_:1次元配列適用後の重み
+    # cost_:各エポックでの誤差平方和のコスト関数
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1) -> None:
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        # 訓練データに適合させる
+        # X:{配列のようなデータ構造},shape={n_examples,n_features},訓練データ
+        # n_examples:訓練データの個数、n_features:特徴量の個数
+        # y:{配列のようなデータ構造}、shape={n_examples}
+        # 戻り値:object
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            # 訓練回数分まで訓練データを反復
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            # 誤差の計算
+            errors = (y-output)
+            # w1.............wmの更新
+            self.w_[1:] += self.eta*X.T.dot(errors)
+            # w0の更新
+            self.w_[0] += self.eta*errors.sum()
+            # コスト関数の計算
+            cost = (errors**2).sum()/2.0
+            # コストの格納
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        # 総入力を計算
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        # 1ステップ後のクラスラベルを返す
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+
+# 描画領域を1行2列に分割
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+# 勾配降下法によるADALINEの学習(学習率 eta=0.01)
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+# エポック数とコストの関係を表す折れ線グラフのプロット(縦軸のコストは常用対数)
+ax[0].plot(range(1, len(ada1.cost_)+1), np.log10(ada1.cost_), marker='o')
+# 軸のラベルの設定
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+# タイトルの設定
+ax[0].set_title('Adaline-Learning rate 0.01')
+# 勾配降下法によるADALINEの学習(学習率 eta=0.0001)
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+# エポック数とコストの関係を表す折れ線グラフのプロット
+ax[1].plot(range(1, len(ada2.cost_)+1), ada2.cost_, marker='o')
+# 軸のラベルの設定
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+# タイトルの設定
+ax[1].set_title('Adaline-Learning rate 0.0001')
 # 図の表示
 plt.show()
