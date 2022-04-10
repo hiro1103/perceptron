95a1c783d82016fb46bf15658d0b2a791cf0a42c
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2022-04-10 19:17:10 +0900

    last

--- Perceptron.py
+++ Perceptron.py
@@ -1,8 +1,8 @@
 from cProfile import label
-from turtle import color
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 class Perceptron(object):
@@ -89,5 +89,50 @@ plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 # 軸のラベルの設定
 plt.xlabel('Epochs')
 plt.ylabel('Number of update')
+# 図の表示
+plt.show()
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # マーカーとカラーマップの準備
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 決定領域のプロット
+    x1_min, x1_max = X[:, 0].min()-1, X[:, 0].max()+1
+    x2_min, x2_max = X[:, 1].min()-1, X[:, 1].max()+1
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+
+    # 各特徴量を1次元配列に変換して予測を実行
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    # 予測結果を元のグリッドポイントのデータサイズに変換
+    Z = Z.reshape(xx1.shape)
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    # 軸の範囲の設定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # クラスごとに訓練データをプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolors='black')
+
+
+# 決定領域のプロット
+plot_decision_regions(X, y, classifier=ppn)
+# 軸のラベルの設定
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+# 凡例の設定（左上に配置）
+plt.legend(loc='upper left')
 # 図の表示
 plt.show()
