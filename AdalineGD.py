9d716d3b8523704813536385f0037dbf248920ba
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2022-04-11 14:40:37 +0900

    完了

--- AdalineGD.py
+++ AdalineGD.py
@@ -1,8 +1,8 @@
+from matplotlib.colors import ListedColormap
+from numpy.random import seed
 import numpy as np
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -174,5 +174,90 @@ plt.plot(range(1, len(ada_gd.cost_)+1), ada_gd.cost_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Sum-squared-error')
 # 図の表示
+plt.tight_layout()
+plt.show()
+
+
+class AdalineSGD(object):
+
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
+
+# 確率的勾配降下法によるADALINEの学習
+ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+# モデルへの適合
+ada_sgd.fit(X_std, y)
+# 境界領域のプロット
+plot_decision_regions(X_std, y, classifier=ada_sgd)
+# タイトルの設定
+plt.title('Adaline-Stochastic Gradient Descent')
+# 軸のラベルの設定
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal lenght [standardized]')
+# 凡例の設定(左上に配置)
+plt.legend(loc='upper left')
+plt.tight_layout()
+# プロットの表示
+plt.show()
+# エポックとコストの折れ線グラフのプロット
+plt.plot(range(1, len(ada_sgd.cost_)+1), ada_sgd.cost_, marker='o')
+# 軸のラベルの設定
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+# プロットの表示
 plt.tight_layout()
 plt.show()
